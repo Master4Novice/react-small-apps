3f4fb1f20cdf9311b78e3de1eb7e5261e106de0d
Neha <Neha@Neha-PC>
2017-12-17 00:37:37 +0530

    Auth User Server Updated

--- server/Apps/src/main/java/com/app/ctrlr/LoginController.java
+++ server/Apps/src/main/java/com/app/ctrlr/LoginController.java
@@ -1,13 +1,31 @@
 package com.app.ctrlr;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.vo.CustomResponse;
+import com.app.vo.User;
+
 
 @RestController
 public class LoginController {
 	
-	@RequestMapping("/hi")
-	public String sayHello() {
-		return "Hello !!";
+	private static final String URL_CROSS = "*";
+	
+	@CrossOrigin(origins=URL_CROSS)
+	@PostMapping("/authUser")
+	public CustomResponse authenticateUser(@RequestBody User user) {
+		String message = "";
+		if(user.getUsername().equals("Admin") && user.getPassword().equals("admin")){
+			message = "Success";
+		}else{
+			message = "Failure";
+		}
+		return new CustomResponse(message);
 	}
 
 }
---
+++ server/Apps/src/main/java/com/app/vo/CustomResponse.java
@@ -0,0 +1,23 @@
+package com.app.vo;
+
+public class CustomResponse {
+
+	private String message;
+
+	public CustomResponse() {
+
+	}
+
+	public CustomResponse(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
